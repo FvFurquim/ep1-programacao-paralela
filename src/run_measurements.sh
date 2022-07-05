7d3f5de3e515f41d9cf930057d1b6712f6f9d0ad
stefanyramos <40060993+stefanyramos@users.noreply.github.com>
2022-07-05 18:12:18 -0300

    update run_measurements.sh - add number of threads in the args

--- src/run_measurements.sh
+++ src/run_measurements.sh
@@ -8,24 +8,28 @@ INITIAL_SIZE=16
 
 SIZE=$INITIAL_SIZE
 
-NAMES=('mandelbrot_seq' 'mandelbrot_pth' 'mandelbrot_omp')
+NAMES=('mandelbrot_pth' 'mandelbrot_omp')
+N_THREADS=('2' '4' '8' '10' '16' '32')
 
 make
 mkdir results
 
 for NAME in ${NAMES[@]}; do
-    mkdir results/$NAME
+    for N_TH in ${N_THREADS[@]}; do
+        mkdir results/$NAME/
+        mkdir results/$NAME/$N_TH
 
-    for ((i=1; i<=$ITERATIONS; i++)); do
-            perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
-            SIZE=$(($SIZE * 2))
+        for ((i=1; i<=$ITERATIONS; i++)); do
+                perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE $N_TH >> full.log 2>&1
+                perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE $N_TH >> seahorse.log 2>&1
+                perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE $N_TH >> elephant.log 2>&1
+                perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $N_TH >> triple_spiral.log 2>&1
+                SIZE=$(($SIZE * 2))
+        done
+
+        SIZE=$INITIAL_SIZE
+
+        mv *.log results/$NAME/$N_TH
+        rm output.ppm
     done
-
-    SIZE=$INITIAL_SIZE
-
-    mv *.log results/$NAME
-    rm output.ppm
 done
